f0dead54e00305cf3c91bcd8e0faf84c5f4e3884
Lindomaromaislindo <lucimar.mi@aluno.ifsc.edu.br>
2024-11-04 17:27:45 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -9,7 +9,52 @@
   <body>
     <?php
     $nome = "Luciusdomarazulciano";
-    echo "Olá mundo!".$nome;
+    $idade = 200;
+    echo "Olá mundo!".$nome. "<br> Idade: ".$idade;
+
+    if($idade <= 18) {
+      echo "Menor de idade ";
+    } else {
+      echo "<br>Maior de idade";
+    }
+
+    $pessoas = [
+      "<br>"."Roberto", "Toatra", "ErrouNoob"
+      
+    ];
+
+    for($i = 0; $i < count($pessoas); $i++) {
+      echo $pessoas[$i]."<br>";
+    }
+
+    $idades = [15, 16, 18];
+
+    foreach($idades as $item) {
+      if($item >= 18) {
+      echo $item."<br>";
+    }
+  }
+
+  $num1 = 5;
+  $num2 = 6;
+  $opcao = 2;
+  switch ($opcao) {
+    case 1:
+      echo "Soma". $num1+$num2;
+    break;
+    case 2:
+      echo "Subtração: ". $num1=$num2;
+    break;
+    case 3:
+     echo "Multiplicação: ". $num1*$num2;
+    break;
+    case 3:
+      echo "Divisão: ". $num1/$num2;
+    break;
+    default:
+    echo "Opção de quando algo da errado, então deu errado";
+   }
+   
     ?>
   </body>
   </html>
